5c5c87b20c54881f429e7da1ec2f84b6a618c6dc
ryan <ryandavidneal@gmail.com>
2019-01-09 18:52:06 +0000

    Initial Spring Boot setup

---
+++ src/main/java/com/github/ryneal/oidc/client/SpringBootOpenidConnectClientApplication.java
@@ -0,0 +1,14 @@
+package com.github.ryneal.oidc.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpenidConnectClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOpenidConnectClientApplication.class, args);
+	}
+
+}
+
